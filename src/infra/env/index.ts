8b076b113c00803d97847d88837bba8dbd08777a
Artur Poffo <arturpoffoc@hotmail.com>
2024-01-18 20:37:43 -0300

    Config: cloudflare R2 and docker for development

--- src/infra/env/index.ts
+++ src/infra/env/index.ts
@@ -11,6 +11,10 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.string(),
+  CLOUDFLARE_ACCOUNT_ID: z.string(),
+  AWS_BUCKET_NAME: z.string(),
+  AWS_ACCESS_KEY_ID: z.string(),
+  AWS_SECRET_ACCESS_KEY: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
